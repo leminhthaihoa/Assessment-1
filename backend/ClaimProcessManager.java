f6b78b8bb5da2b971dd9c84e3c6706f231c05c9c
s3979194 <s3979194@rmit.edu.vn>
2024-04-01 09:10:55 +0700

    Create claim process manager interface

--- backend/ClaimProcessManager.java
+++ backend/ClaimProcessManager.java
@@ -1,4 +1,12 @@
 package backend;
 
 public interface ClaimProcessManager {
+    public void addClaim(Claim claim);
+    public void updateClaim(Claim claim, int ind);
+
+    public void deleteClaim(int ind);
+
+    public void getOne(Claim claim);
+
+    public void getAll();
 }
